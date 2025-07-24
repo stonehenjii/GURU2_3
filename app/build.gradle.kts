0bd381f65a22c417e32720fcf0dbbe1ab7224755
stonehenjii <2015yk1207@gmail.com>
2025-07-24 10:04:31 +0900

    sdk version change

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.guru2_3"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.guru2_3"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
