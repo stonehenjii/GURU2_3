c502745969e020a8d6e76d16f408ef6f85b830b0
juhyung <njh1019@gmail.com>
2025-07-27 12:37:05 +0900

    타이머 파일 추가

--- app/src/main/java/com/example/guru2_3/MainActivity.kt
+++ app/src/main/java/com/example/guru2_3/MainActivity.kt
@@ -1,20 +1,101 @@
 package com.example.guru2_3
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.Timer
+import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
+
+    private var time = 0;
+    private var timerTask : Timer? = null
+    private var isRunning = false
+    private var studyTime = 25
+    private var shortBreak = 5
+    private var longBreak = 15
+    private var session = 8
+    private var isTimeSet = false
+
+    private var sessionCount = 0
+    private var isBreak = false
+
+    lateinit var  minTextView : TextView
+    lateinit var  secTextView: TextView
+    lateinit var  setButton: ImageButton
+    lateinit var  pauseButton: ImageButton
+    lateinit var  resetButton: ImageButton
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // enableEdgeToEdge() // 이 부분은 최신 UI 기능으로, 필요 없다면 주석 처리하거나 삭제해도 됩니다.
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        //값을 가져옵니다.
+        studyTime = intent.getIntExtra("집중시간", 25)
+        shortBreak = intent.getIntExtra("짧은휴식", 5)
+        longBreak = intent.getIntExtra("긴휴식", 15)
+        session = intent.getIntExtra("세션수", 8)
+        Log.d("MainActivity", "studyTime: $studyTime, shortBreak: $shortBreak, longBreak: $longBreak, session: $session")
+
+        // 상태바 색 변경 코드 추가
+        // window.statusBarColor = ContextCompat.getColor(this, R.color.mainRed)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR  // 상태바 아이콘을 검정색으로
+
+        //툴바를 추가합니다
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        //타이머를 설정합니다
+        secTextView = findViewById(R.id.secTextView)
+        minTextView = findViewById(R.id.minTextView)
+        setButton = findViewById(R.id.setButton)
+        pauseButton = findViewById(R.id.pauseButton)
+        resetButton = findViewById(R.id.resetButton)
+
+
+        setButton.setOnClickListener {
+            if (!isTimeSet) {
+                // ⏱️ 상태에 따라 시간 설정
+                time = when {
+                    isBreak && sessionCount  >= session -> longBreak * 60 * 100  // 긴 휴식
+                    isBreak -> shortBreak * 60 * 100                           // 짧은 휴식
+                    else -> studyTime * 60 * 100                               // 공부
+                }
+                isTimeSet = true
+            }
+
+            setButton.visibility = View.GONE
+            resetButton.visibility = View.GONE
+            pauseButton.visibility = View.VISIBLE
+            start()
+
+        }
+
+        pauseButton.setOnClickListener {
+            setButton.visibility = View.VISIBLE
+            resetButton.visibility = View.VISIBLE
+            pauseButton.visibility = View.GONE
+            pause()
+        }
+
+        resetButton.setOnClickListener {
+            reset()
         }
 
         // --- 여기에 DatabaseHelper 관련 코드 추가 ---
@@ -33,4 +114,104 @@ class MainActivity : AppCompatActivity() {
         }
         // ------------------------------------
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_top, menu)
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.profile -> {
+                // 사용자 설정 눌렀을 때 동작
+                return true
+            }
+            R.id.timerSetting -> {
+                // 타이머 설정 눌렀을 때 동작
+                val intent = Intent(this, TimerSetting::class.java)
+                intent.putExtra("집중시간", studyTime)
+                intent.putExtra("짧은휴식", shortBreak)
+                intent.putExtra("긴휴식", longBreak)
+                intent.putExtra("세션수", session)
+                startActivity(intent)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun pause(){
+        timerTask?.cancel()
+        isRunning = false
+    }
+
+    private fun start(){
+        timerTask = timer(period = 10) {
+            time--
+
+            val totalSec = time / 100
+            val min = totalSec / 60
+            val sec = totalSec % 60
+
+            if (time <= 0) {
+                timerTask?.cancel()
+                isRunning = false
+
+                runOnUiThread {
+                    setButton.visibility = View.VISIBLE
+                    resetButton.visibility = View.VISIBLE
+                    pauseButton.visibility = View.GONE
+                }
+
+                if (!isBreak) {
+                    sessionCount = 0
+                    sessionCount++
+                    if (sessionCount  >= session) {
+                        time = longBreak * 60 * 100
+                    } else {
+                        time = shortBreak * 60 * 100
+                    }
+                    isBreak = true
+                } else {
+                    if (sessionCount >= session) {
+                        runOnUiThread {
+                            Toast.makeText(this@MainActivity, "모든 세션이 완료되었습니다!", Toast.LENGTH_SHORT).show()
+                        }
+                    } else {
+                        time = studyTime * 60 * 100
+                        isBreak = false
+                    }
+                }
+
+                // 👉 사용자가 다시 재생 버튼 누를 때까지 기다리기
+                isTimeSet = false
+            }
+
+            runOnUiThread {
+                minTextView.text = String.format("%02d", min)
+                secTextView.text = String.format("%02d", sec)
+            }
+        }
+        isRunning = true
+    }
+
+    private fun reset(){
+        timerTask?.cancel()
+        isRunning = false
+        isTimeSet = false
+        isBreak = false
+        sessionCount = 0
+        // 시간 초기화
+        time = studyTime * 60 * 100
+        val totalSec = time / 100
+        val min = totalSec / 60
+        val sec = totalSec % 60
+
+        runOnUiThread {
+            minTextView.text = String.format("%02d", min)
+            secTextView.text = String.format("%02d", sec)
+        }
+
+    }
+
 }
---
+++ app/src/main/java/com/example/guru2_3/TimerSetting.kt
@@ -0,0 +1,83 @@
+package com.example.guru2_3
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class TimerSetting : AppCompatActivity() {
+
+    var studyTime = 25
+    var shortBreak = 5
+    var longBreak = 15
+    var session = 8
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_timer_setting)
+
+        studyTime = intent.getIntExtra("집중시간", 25)
+        shortBreak = intent.getIntExtra("짧은휴식", 5)
+        longBreak = intent.getIntExtra("긴휴식", 15)
+        session = intent.getIntExtra("세션수", 8)
+
+        // 상태바 색 변경 코드 추가
+        // window.statusBarColor = ContextCompat.getColor(this, R.color.mainRed)
+        window.decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR  // 상태바 아이콘을 검정색으로
+
+        val studyTimeEt = findViewById<EditText>(R.id.studyTimeEt)
+        val shortBreakEt = findViewById<EditText>(R.id.shortBreakEt)
+        val longBreakEt = findViewById<EditText>(R.id.longBreakEt)
+        val sessionEt = findViewById<EditText>(R.id.sessionEt)
+        val saveBtn = findViewById<ImageButton>(R.id.SaveBtn)
+
+
+        studyTimeEt.setText(studyTime.toString())
+        shortBreakEt.setText(shortBreak.toString())
+        longBreakEt.setText(longBreak.toString())
+        sessionEt.setText(session.toString())
+
+        saveBtn.setOnClickListener {
+            studyTime = studyTimeEt.text.toString().toIntOrNull() ?: 25
+            shortBreak = shortBreakEt.text.toString().toIntOrNull() ?: 5
+            longBreak = longBreakEt.text.toString().toIntOrNull() ?: 15
+            session = sessionEt.text.toString().toIntOrNull() ?: 8
+
+            if (studyTime < 0 || studyTime > 60) {
+                Toast.makeText(this, "집중시간: 0~60 사이 숫자만 입력 가능해요", Toast.LENGTH_SHORT).show()
+                studyTimeEt.setText("25") // 기존 값으로 복구
+            } else if (shortBreak < 0 || shortBreak > 60){
+                Toast.makeText(this, "짧은 휴식: 0~60 사이 숫자만 입력 가능해요", Toast.LENGTH_SHORT).show()
+                shortBreakEt.setText("5") // 기존 값으로 복구
+            } else if (longBreak < 0 || longBreak > 60){
+                Toast.makeText(this, "긴 휴식: 0~60 사이 숫자만 입력 가능해요", Toast.LENGTH_SHORT).show()
+                longBreakEt.setText("15") // 기존 값으로 복구
+            } else if (session < 1 || session > 10){
+                Toast.makeText(this, "세션: 1~10 사이 숫자만 입력 가능해요", Toast.LENGTH_SHORT).show()
+                sessionEt.setText("8") // 기존 값으로 복구
+            } else {
+                val intent = Intent(this, MainActivity::class.java)
+                intent.putExtra("집중시간", studyTime)
+                intent.putExtra("짧은휴식", shortBreak)
+                intent.putExtra("긴휴식", longBreak)
+                intent.putExtra("세션수", session)
+                startActivity(intent)
+                Log.d(
+                    "TimerSetting",
+                    "studyTime: $studyTime, shortBreak: $shortBreak, longBreak: $longBreak, session: $session"
+                )
+            }
+
+        }
+
+
+    }
+}
